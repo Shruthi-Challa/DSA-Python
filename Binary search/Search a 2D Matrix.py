39488329e4c7bea94836c3ae4615d2ddf5993ab7
Shruthi-Challa <challashruthi94@gmail.com>
2026-08-19 06:44:23 +0530

    added file

---
+++ Binary search/Search a 2D Matrix.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        left = 0
+        right = len(matrix)*len(matrix[0]) - 1
+
+        while left <= right:
+            mid= left+(right-left)//2
+
+            row = mid // len(matrix[0])
+            col = mid % len(matrix[0])
+
+            if matrix[row][col]==target:
+                return True
+
+            elif matrix[row][col] < target:
+                left = mid+1
+
+            else:
+                right = mid-1
+
+        return False
